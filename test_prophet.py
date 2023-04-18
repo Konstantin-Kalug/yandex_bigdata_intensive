5d4022abad5251c83c24e2774f866ec9b297ab40
NikitaMulyar <ni***ol@mail.ru>
2023-04-19 01:06:50 +0300

    Добавлены прогнозы v1

--- test_prophet.py
+++ test_prophet.py
@@ -1,16 +1,7 @@
-from pyspark import SparkConf, SparkContext
-import os
-import datetime
-import shutil
 import pandas as pd
-import numpy as np
-import requests
-import networkx as nx
-from IPython.display import Image
-import matplotlib.pyplot as plt
-from math import floor
 from prophet import Prophet
-
+from datetime import datetime
+import pystan
 """
 ПРОГНОЗЫ
 """
@@ -19,16 +10,27 @@ from prophet import Prophet
 
 members = []
 casuals = []
-for year in range(2013, 2024):
+st = 2013
+end = 2023
+for year in range(st, end):
     df = pd.read_table(f'data/src/{year}/part-00000', index_col=False, header=None, names=['id', 'biketype', 'starttime', 'endtime', 'startstation', 'endstation', 'startlat', 'startlng', 'endlat', 'endlng', 'member_casual', 'gender', 'age', 'bikeid'], sep=',')
     casuals.append(len(df.loc[(df['member_casual'] == 'Customer') | (df['member_casual'] == 'casual')]))
     members.append(len(df.loc[(df['member_casual'] == 'Subscriber') | (df['member_casual'] == 'member')]))
-plt.plot(range(2013, 2024), casuals)
-plt.plot(range(2013, 2024), members)
-plt.xlabel('Года наблюдений')
-plt.ylabel('Количество поездок, млн.')
-plt.legend(['Нечлены', 'Члены'])
-plt.title('Динамическое изменение количества членов и нечленов')
-plt.show()
+    print(year)
+df_casuals = pd.DataFrame(data={'y': casuals, 'ds': [datetime(year=i, month=1, day=1) for i in
+                                                     range(st, end)]})
+df_members = pd.DataFrame(data={'y': members, 'ds': [datetime(year=i, month=1, day=1) for i in
+                                                     range(st, end)]})
+print(df_casuals)
+m_casuals = Prophet()
+m_casuals.fit(df_casuals)
+future_casuals = m_casuals.make_future_dataframe(periods=5, freq='Y')
+forecast_casuals = m_casuals.predict(future_casuals)
+print(forecast_casuals[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
 
+m_members = Prophet()
+m_members.fit(df_casuals)
+future_members = m_members.make_future_dataframe(periods=5, freq='Y')
+forecast_members = m_casuals.predict(future_members)
+print(forecast_members[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
 # ===========
